93a5b17a7081cd384aaf93b84d0238b6cf762f35
Volodymyr Ihovin <valdihor@gmail.com>
2021-01-27 10:26:57 -0800

    Global changes for routes, app, models

---
+++ sanic/api.py
@@ -0,0 +1,33 @@
+from sanic import Blueprint
+
+from resources import MainPageView, RolesView, RoleByIdView, UsersView, UserByIdView, EventsView, EventByIdView, \
+    StagesView, StageByIdView, PerformancesView, PerformanceByIdView, MaterialsView, MaterialByIdView
+
+main_page = Blueprint('main_page', url_prefix="/")
+main_page.add_route(MainPageView.as_view(), '/',  strict_slashes=False)
+
+roles = Blueprint('roles', url_prefix='/roles', strict_slashes=False)
+roles.add_route(RolesView.as_view(), '/',  strict_slashes=False)
+roles.add_route(RoleByIdView.as_view(), '/<role_id>',  strict_slashes=False)
+
+users = Blueprint('users', url_prefix='/users', strict_slashes=False)
+users.add_route(UsersView.as_view(), '/',  strict_slashes=False)
+users.add_route(UserByIdView.as_view(), '/<user_id>',  strict_slashes=False)
+
+events = Blueprint('events', url_prefix='/events', strict_slashes=False)
+events.add_route(EventsView.as_view(), '/',  strict_slashes=False)
+events.add_route(EventByIdView.as_view(), '/<event_id>',  strict_slashes=False)
+
+stages = Blueprint('stages', url_prefix='/stages', strict_slashes=False)
+stages.add_route(StagesView.as_view(), '/',  strict_slashes=False)
+stages.add_route(StageByIdView.as_view(), '/<stage_id>',  strict_slashes=False)
+
+performances = Blueprint('performances', url_prefix='/performances', strict_slashes=False)
+performances.add_route(PerformancesView.as_view(), '/',  strict_slashes=False)
+performances.add_route(PerformanceByIdView.as_view(), '/<performance_id>',  strict_slashes=False)
+
+materials = Blueprint('materials', url_prefix='/materials', strict_slashes=False)
+materials.add_route(MaterialsView.as_view(), '/',  strict_slashes=False)
+materials.add_route(MaterialByIdView.as_view(), '/<material_id>',  strict_slashes=False)
+
+api = Blueprint.group(main_page, roles, users, events, stages, performances, materials, url_prefix='/')
--- sanic/app.py
+++ sanic/app.py
@@ -1,21 +1,7 @@
-from sanic import Sanic
 from gino.ext.sanic import Gino
-import asyncio
+from sanic import Sanic
 
 
 app = Sanic(name='event_organizer')
-app.config.DB_HOST = 'localhost'
-app.config.DB_DATABASE = 'gino'
-app.config['DB_USER'] = 'postgres'
-app.config['DB_PASSWORD'] = 'thisisnewsecurepassword'
-
-
+app.config.from_envvar('MYAPP_SETTINGS')
 db = Gino()
-db.init_app(app)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(db.set_bind('postgresql://postgres:thisisnewsecurepassword@localhost/gino'))
-from models import User, Role, Event, Stage, Performance, Material
-loop.run_until_complete(db.gino.create_all())
-
-
-app.static('/static', './static')
