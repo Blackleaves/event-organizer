c22be4fbfa21547f8bab0387006f2cb029c1436f
Volodymyr Ihovin <valdihor@gmail.com>
2021-02-10 09:26:32 -0800

    Docker updates

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,14 @@ FROM python:3.8.5
 RUN mkdir -p /usr/src/app/
 WORKDIR /usr/src/app/
 
+RUN apt-get update
+RUN apt-get -y install gcc
+
 COPY . /usr/src/app/
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 8080
+EXPOSE 8000
 
 ENV MYAPP_SETTINGS /usr/src/app/sanic/config.py
 
-CMD ["python","sanic/main.py","--host='0.0.0.0'","--port=8000"]
+CMD ["python","sanic/main.py","--host=127.0.0.1","--port=8000"]
--- sanic/main.py
+++ sanic/main.py
@@ -1,22 +1,28 @@
 import argparse
+import os
 
 from api import api
 from app import app, db
 from models import User, Role, Event, Stage, Performance, Material
 
 # TO RUN USE THIS
-# MYAPP_SETTINGS=~/Mentoree/event-organizer/sanic/config.py python sanic/main.py --host="0.0.0.0" --port=8000
+# MYAPP_SETTINGS=~/Mentoree/event-organizer/sanic/config.py python sanic/main.py --host="0.0.0.0" --port=8080
 
 parser = argparse.ArgumentParser("Parse arguments")
 parser.add_argument("--host", type=str, required=True, help="DB Host")
 parser.add_argument("--port", required=True, help="DB Host Port")
 
+db_string = "postgresql://{}:{}@{}/{}".format(os.getenv('DB_USER', 'vihovin'),
+                                              os.getenv('DB_PASSWORD', 'thisissomenewpass'),
+                                              os.getenv('DB_HOST', 'localhost'),
+                                              os.getenv('PG_DATABASE', 'gino'))
+
 
 @app.listener('before_server_start')
 async def before_server_start(app, loop):
     db.init_app(app)
-    # await db.set_bind('postgresql://vihovin:thisissomenewpass@localhost/gino')
-    # await db.gino.create_all()
+    await db.set_bind(db_string)
+    await db.gino.create_all()
     app.blueprint(api)
 
 
